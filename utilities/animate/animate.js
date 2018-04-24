69e812c2ce2e2c16eb906ac217895e7685a7bed6
Mattias Haal <mattias.haal@grandpublic.se>
2018-04-24 09:52:05 +0200

    Added animate
    Animate is a function for optimizing the use of requestAnimationFrame.

---
+++ utilities/animate/animate.js
@@ -0,0 +1,34 @@
+/**
+ * Animate the view with requestAnimationFrame.
+ *
+ * @param {Event} type - Listen for specified event
+ * @param {Node} element - Element to check the event against
+ * @param {Function} callback - Callback function to be executed
+ * @param {Boolean} capture - Propagating event
+ * @return {Function} - The animated function
+ */
+const animate = (type, element, callback, capture) => {
+  const raf = window.requestAnimationFrame;
+  let ticking = false;
+
+  // Callback for the event:
+  const render = () => {
+    if (!ticking) {
+      ticking = true;
+      raf(update);
+    }
+  };
+
+  // Animation callback:
+  const update = () => {
+    if (ticking) {
+      callback();
+    }
+
+    // Reset the tick, so we can capture the next event:
+    ticking = false;
+  };
+
+  // Listen for events:
+  element.addEventListener(type, render, !!capture);
+};
